7e2e20df8ebd27ad50e2971522c6b20451612f18
Jay <dingorkar.j@gmail.com>
2019-10-06 20:44:11 -0400

    refactor(maven): update project structure to build jar

--- src/test/java/FiniteStateMachineTest.java
+++ src/test/java/FiniteStateMachineTest.java
@@ -1,3 +1,4 @@
+import com.orka.FiniteStateMachine;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,32 +11,32 @@ public class FiniteStateMachineTest {
     List<String> validFinalStates = Arrays.asList("S0", "S1", "S2");
 
     @Test
-    public void test_currentStateS0_input0_resultStateS0_OfValue0(){
+    public void test_currentStateS0_input0_resultStateS0_OfValue0() {
         Assert.assertTrue(fsm.run("0", "S0", validFinalStates).equalsIgnoreCase("0"));
     }
 
     @Test
-    public void test_currentStateS0_input1_resultStateS1_OfValue1(){
+    public void test_currentStateS0_input1_resultStateS1_OfValue1() {
         Assert.assertTrue(fsm.run("1", "S0", validFinalStates).equalsIgnoreCase("1"));
     }
 
     @Test
-    public void test_currentStateS1_input1_resultStateS2_OfValue2(){
+    public void test_currentStateS1_input1_resultStateS2_OfValue2() {
         Assert.assertTrue(fsm.run("0", "S1", validFinalStates).equalsIgnoreCase("2"));
     }
 
     @Test
-    public void test_currentStateS1_input1_resultStateS0_OfValue0(){
+    public void test_currentStateS1_input1_resultStateS0_OfValue0() {
         Assert.assertTrue(fsm.run("1", "S1", validFinalStates).equalsIgnoreCase("0"));
     }
 
     @Test
-    public void test_currentStateS2_input0_resultStateS1_OfValue1(){
+    public void test_currentStateS2_input0_resultStateS1_OfValue1() {
         Assert.assertTrue(fsm.run("0", "S2", validFinalStates).equalsIgnoreCase("1"));
     }
 
     @Test
-    public void test_currentStateS2_input1_resultStateS2_OfValue2(){
+    public void test_currentStateS2_input1_resultStateS2_OfValue2() {
         Assert.assertTrue(fsm.run("1", "S2", validFinalStates).equalsIgnoreCase("2"));
     }
 }
